146fc6226dc64195707b91dfa21092e0c08e8dda
Shafaf <shafaf@zennode.com>
2023-09-29 19:29:44 +0530

    UI updated

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 
 import "./home.css"
 import ADDIMG from "../Assets/image/ad-banner-sample.png"
+import AMNICON from "../Assets/image/amazon-png-logo-vector-6701.png"
+
 
 function Home() {
   return (
@@ -19,8 +21,26 @@ export default Home;
 
 const BodyList = () => {
   return (
-    <div className="row mt-5">
-      
+    <div className="row mt-5 list-item-wrapper">
+      <div className="col-md-6 list-card m-3 p-4">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>Amazon</h4>
+            <button className='btn btn-primary '>
+            <a className="text-light"href="">Go to Amazon</a></button>
+          </div>
+          <div className="col-md-6 img-brand-logo">
+            <img src={AMNICON} alt="" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-5"></div>
+          <div className="col-md-7"></div>
+        </div>
+      </div>
+      <div className="col-md-6 list-card m-3 p-4">
+        hii
+      </div>
     </div>
   )
 }
@@ -36,8 +56,9 @@ const SearchBar = () => {
       <div className="col-md-2">
         <SwitchTabber
           options={[
-            { value: 'grid', icone : true },
-            { value: 'hor', icone : false  },
+            { value: 'list' },
+            { value: 'grid' },
+            { value: 'hor' },
           ]}
           defaultValue='tele_in'
         />
@@ -46,7 +67,6 @@ const SearchBar = () => {
     </div>
   )
 }
-
 
 const SwitchTabber= (props) => { 
   const { options, name, defaultValue } = props;
@@ -59,11 +79,14 @@ const SwitchTabber= (props) => {
           options.map((option, index) => {
             const grid = addClass === "grid" ? "active" : "";
             const hor = addClass === "hor" ? "active" : "";
+            const list = addClass === "list" ? "active" : "";
             return(
               <>
                 <input type="radio" name='hai' id={option.value} defaultChecked={true} onClick={()=>setAddClass(option.value)}/>
                 <label htmlFor={ option.value } className="label">
-                  {option?.icone ? <i className={`bi bi-grid-fill ${grid}`}></i> : <i class={`bi bi-square-half ${hor}`}></i>}
+                  {option?.value === "grid" && <i className={`bi bi-grid-fill ${grid}`}></i>}
+                  {option?.value === "hor" && <i class={`bi bi-square-half ${hor}`}></i>}
+                  {option?.value === "list" && <i class={`bi bi-grid-3x3-gap-fill ${list}`}></i>}
                 </label>
               </>
             )
@@ -76,8 +99,6 @@ const SwitchTabber= (props) => {
     
   );
 };
-
-
 
 const AdNotice = () =>{
   return (
